abef1801de54b8ffa0f174303a3cc172f018b227
JonFillip <tjayphil14@gmail.com>
2020-02-29 13:18:09 +0200

    added new files to gitignore

--- django_app/settings.py
+++ django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7*6v_w&104n$r04qij1hyve7g333&*h7f=!@7lqbp%fmvb)yl$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mystudytracker.herokuapp.com']
 
 
 # Application definition
